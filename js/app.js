9f5fd6abf233f5f98977b867e2b4d1a2ee29444b
MariaChebanova <chebanova.maria@gmail.com>
2015-11-14 01:23:15 -0800

    email and passwors validation are added. Looks bad though

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
 'use strict'
 
+//function that checks if passwords match
+function checkPass() {
+    //Store the password field objects into variables ...
+    var password = document.getElementById('password');
+    var confirmPassword = document.getElementById('confirmPassword');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var noColor = "#ffffff";
+    var redColor = "#ff6666";
+    //Compare the values in the password field
+    //and the confirmation field
+    if(password.value == confirmPassword.value){
+        //The passwords match.
+        //Set the color to noColor and inform
+        //the user that they have entered the correct password
+        confirmPassword.style.backgroundColor = noColor;
+        message.style.color = noColor;
+//        message.innerHTML = "Passwords Match!"
+    }else{
+        //The passwords do not match.
+        //Set the color to the red color and
+        //Set the color to the red color and
+        //notify the user.
+        confirmPassword.style.backgroundColor = redColor;
+        message.style.color = redColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+}
 
 angular.module('formSubmission', [])
 
