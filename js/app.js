38107ab628955c0fe0b7ab2575800705274d8e44
Joe Pollock <c.joseph.pollock@gmail.com>
2015-11-17 14:30:58 -0800

    passwords do not match fixed

--- js/app.js
+++ js/app.js
@@ -1,39 +1,13 @@
 'use strict'
 
 
-    //function that checks if passwords match
-    function checkPass() {
-        //Store the password field objects into variables ...
-        var password = document.getElementById('password');
-        var confirmPassword = document.getElementById('confirmPassword');
-        //Store the Confimation Message Object ...
-        var message = document.getElementById('confirmMessage');
-        //Set the colors we will be using ...
-        var noColor = "#ffffff";
-        var redColor = "#ff6666";
-        //Compare the values in the password field
-        //and the confirmation field
-        if (password.value == confirmPassword.value) {
-            //The passwords match.
-            //Set the color to noColor and inform
-            //the user that they have entered the correct password
-            confirmPassword.style.backgroundColor = noColor;
-            message.style.color = noColor;
-                    // message.innerHTML = "Passwords Match!"
-        } else {
-            //The passwords do not match.
-            //Set the color to the red color and
-            //Set the color to the red color and
-            //notify the user.
-            confirmPassword.style.backgroundColor = redColor;
-            message.style.color = redColor;
-            message.innerHTML = "Passwords Do Not Match!"
-        }
-    }
+
 
 angular.module('formSubmission', [])
 
 .controller('formCtrl', ['$scope', function($scope) {
+
+    $scope.showPasswordsDoNotMatch = false;
 
     $scope.submitForm = function() {
         $scope.submitted = true;
@@ -43,6 +17,48 @@ angular.module('formSubmission', [])
         $scope.submitted = false;
         $scope.userForm.$setPristine();
     }
+
+
+    //function that checks if passwords match
+    $scope.checkPass = function() {
+        //Store the password field objects into variables ...
+        var password = $scope.user.password;
+        var confirmPassword = $scope.user.confirmPassword;
+
+        if(password === confirmPassword) {
+            $scope.showPasswordsDoNotMatch = false;
+        } else {
+            $scope.showPasswordsDoNotMatch = true;
+        }
+
+        //Store the Confimation Message Object ...
+        // var message = document.getElementById('confirmMessage');
+        // //Set the colors we will be using ...
+        // var noColor = "#ffffff";
+        // var redColor = "#ff6666";
+        // //Compare the values in the password field
+        // //and the confirmation field
+        // if (password.value == confirmPassword.value) {
+        //     //The passwords match.
+        //     //Set the color to noColor and inform
+        //     //the user that they have entered the correct password
+        //     confirmPassword.style.backgroundColor = noColor;
+        //     message.style.color = noColor;
+        //             // message.innerHTML = "Passwords Match!"
+        // } else {
+        //     //The passwords do not match.
+        //     //Set the color to the red color and
+        //     //Set the color to the red color and
+        //     //notify the user.
+        //     confirmPassword.style.backgroundColor = redColor;
+        //     message.style.color = redColor;
+        //     message.innerHTML = "Passwords Do Not Match!"
+        // }
+    }
+
+
+
+
 
 }]);
  function checkBirth() {
