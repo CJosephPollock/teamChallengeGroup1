19a70061a6e80ede9d32504f59dfef1c2639960f
Tianai Zhao <zhang201pig@126.com>
2015-11-17 13:53:37 -0800

    test for lastname, birthday

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,17 +1,47 @@
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    browser.get('https://angularjs.org');
+'use strict';
 
-    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    element(by.css('[value="add"]')).click();
 
-    var todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(3);
-    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+describe('Sign Up Page', function(){
+    var lastName = element(by.model('lastName'));
+    var birthday = element(by.model('birthday'));
+    beforeEach(function(){
+        browser.get('http://localhost:8000');
+    });
 
-    // You wrote your first test, cross it off the list
-    todoList.get(2).element(by.css('input')).click();
-    var completedAmount = element.all(by.css('.done-true'));
-    expect(completedAmount.count()).toEqual(2);
-  });
+    it('should show the correct tilte', function(){
+        
+        expect(browser.getTitle()).toEqual('Our cool website');
+        browser.getTitle().then(console.log);
+    });
+    
+
+    it('should be able to test whether the user type in a last name', function(){
+
+        lastName.sendKeys('zhao');
+        var actual = element(by.css('.errorLN')).getText();
+
+        var expected = "We need to know your last name!";
+        expect(actual).not.toEqual(expected);
+        // lastName.clear();
+        // lastName.sendKeys('');
+        // actual = element(by.css('.errorLN')).getText();
+        // expect(actual).toEqual(expected);
+        
+
+    });
+    it('should be able to test whether the user is older than 13 years old', function(){
+        birthday.sendKeys('04-07-1995');
+        var actual = element(by.css('.validAge')).getText();
+        var expected = "Sorry, you must be at least 13 to sign up!";
+        expect(actual).not.toEqual(expected);
+        birthday.clear();
+        birthday.sendKeys('04-07-2014');
+        actual = element(by.css('.validAge')).getText();
+        expect(actual).toEqual(expected);
+        
+    });
+
+
+    
+
 });
