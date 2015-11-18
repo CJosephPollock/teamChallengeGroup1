7de5c23c45857f3df4189e83efec43ce253fcc17
MariaChebanova <chebanova.maria@gmail.com>
2015-11-17 21:32:54 -0800

    comments added to js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 'use strict'
+//module formSubmission
 angular.module('formSubmission', [])
 
+//controller for the module
 .controller('formCtrl', ['$scope', function($scope) {
-
+    //default values
     $scope.showPasswordsDoNotMatch = false;
-
+    //defining submitForm
     $scope.submitForm = function() {
+        //default value within scope
         $scope.submitted = true;
 
     }
+
     $scope.resetForm = function() {
         $scope.submitted = false;
         $scope.userForm.$setPristine();
@@ -22,7 +26,7 @@ angular.module('formSubmission', [])
         //Store the password field objects into variables ...
         var password = $scope.user.password;
         var confirmPassword = $scope.user.confirmPassword;
-
+        //Compare the values in the password field
         if(password === confirmPassword) {
             $scope.showPasswordsDoNotMatch = false;
         } else {
@@ -31,11 +35,12 @@ angular.module('formSubmission', [])
     }
     //Check if the input birthday meet the requirements
     $scope.checkBirth = function() {
+    //Store birthday field objects into variables a and define them...
         var birthday = $scope.birthday;
         var birthdayDate = moment(birthday, "MM-DD-YYYY");
         var currentDate = moment();
         var difference = currentDate.diff(birthdayDate, 'years');
-
+        //after calculating, compare the difference and show appropriate message
         $scope.userForm.birthday.$setValidity('tooyoung', true);
         if(difference < 13){
             $scope.userForm.birthday.$setValidity('tooyoung', false);
