7859be9057f29c31bb6a6c5001083b153bc7df68
Dominik Zmuda <dzmuda@uw.edu>
2015-11-17 18:32:02 -0800

    fixing maria's broken change

--- js/app.js
+++ js/app.js
@@ -17,40 +17,8 @@ angular.module('formSubmission', [])
         $scope.submitted = false;
         $scope.userForm.$setPristine();
         $scope.showPasswordsDoNotMatch = false;
-
     }
 
-
-    //function that checks if passwords match
-    $scope.checkPass = function() {
-        //Store the password field objects into variables ...
-        var password = $scope.user.password;
-        var confirmPassword = $scope.user.confirmPassword;
-
-        if(password === confirmPassword) {
-            $scope.showPasswordsDoNotMatch = false;
-        } else {
-            $scope.showPasswordsDoNotMatch = true;
-        }
-    }
-
-
-
-
-    //Check if the input birthday meet the requirements
-    $scope.checkBirth = function() {
-        var birthday = $scope.birthday;
-        var birthdayDate = moment(birthday, "MM-DD-YYYY");
-        var currentDate = moment();
-        var difference = currentDate.diff(birthdayDate, 'years');
-
-        $scope.userForm.birthday.$setValidity('tooyoung', true);
-        if(difference < 13){
-            $scope.userForm.birthday.$setValidity('tooyoung', false);
-        }
-    }
-
-    }
 
     //function that checks if passwords match
     $scope.checkPass = function() {
